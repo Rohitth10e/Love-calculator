1d82b4f636c02e759f43b9505405e056358de59f
Rohith Eshwar S <121311843+Rohitth10e@users.noreply.github.com>
2024-04-21 17:03:08 +0530

    Add files via upload

--- lovecal.js
+++ lovecal.js
@@ -6,8 +6,11 @@ let btn = document.querySelector(".ip");
 function calculateLove(){
     let name1 = document.querySelector(".name1").value;
     let name2 = document.querySelector(".name2").value;
-    disp.innerText = ` Love percentage between ${name1} and ${name2} is ${Math.floor(Math.random()*100+1)}😉❤️`;
-    console.log(name1,name2)
+    if(name1==="" && name2===""){
+        disp.innerTexr="Please enter names"
+    }else{
+        disp.innerText = ` Love percentage between ${name1} and ${name2} is ${Math.floor(Math.random()*100+1)}😉❤️`;
+    }
 }
 
 btn.addEventListener("click", calculateLove);
